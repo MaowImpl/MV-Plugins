4e966d0c3c9ac2d86fe8d398491e6efaef3cf026
Maow <28311382+MaowImpl@users.noreply.github.com>
2021-01-11 19:56:40 -0500

    Create Template.js

---
+++ Template.js
@@ -0,0 +1,37 @@
+// Template file for my plugins, you may use this as well if you like my formatting style, just make sure to change the @author
+
+// ===================================
+//  .js
+// ===================================
+
+/*:
+ * @author       Maow
+ * @plugindesc   
+ * 
+ * @help
+ * -----------------------------------
+ *  Terms of Use
+ * -----------------------------------
+ * 1. Do not try to claim this as your own work.
+ * 2. Attribution is not required, but it is preferred.
+ * 3. Free-use for commercial and noncommercial projects.
+ * -----------------------------------
+ *  Plugin Commands
+ * -----------------------------------
+ *
+ *
+ * -----------------------------------
+ *  Calls
+ * -----------------------------------
+ *
+ *
+ */
+
+// var Namespace = Namespace || {}; -- NAMESPACE FOR PLUGIN CALLS (this has to be a var or else it's not added to the global scope without additional effort)
+
+(function() {
+	'use strict';
+
+	// const params = PluginManager.parameters(<filename>); -- PARAMETERS (object that contains your parameters, the keys of the parameters are the param names)
+	// const param_example = params['example']; 
+})();
